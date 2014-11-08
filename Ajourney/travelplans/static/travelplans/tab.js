d04a3184150004a0d9378fdacc5a96083917def3
Lixin Yao <Staniel@Lixins-MacBook-Pro.local>
2014-11-08 01:11:30 -0500

    add js file

---
+++ Ajourney/travelplans/static/travelplans/tab.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#allplan').click(function(){
+       alert(1);
+    });
+    $('#myplan').click(function(){
+       alert(2);
+    });
+    $('#jplan').click(function(){
+       alert(3);
+    });    
+});
