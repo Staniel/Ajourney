bc3ec22267d7c385b4cb1396cd28a2ad79a539ff
Yu Wang <yw2684@columbia.edu>
2014-11-07 22:34:53 -0500

    Modify views.

--- Ajourney/travelplans/views/view_plans.py
+++ Ajourney/travelplans/views/view_plans.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from travelplans.plan_manager import get_plans_by_destination
+from travelplans.plan_manager import get_plans_by_destination,get_all_plans
 
 def view_my_plans(request):
     return HttpResponse("all my plans")
 
 def view_available_plans(request):
-	plan_list=get_plans_by_destination('Los Angeles')
+	plan_list=get_all_plans()
 	template = loader.get_template('travelplans/view_plans.html')
 	context = RequestContext(request, {
         'plan_list': plan_list,
