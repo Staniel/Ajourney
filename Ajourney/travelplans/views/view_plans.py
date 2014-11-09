ec8d6766af557d0e48a7446c0878f44d453b13ba
Yu Wang <yw2684@columbia.edu>
2014-11-09 04:33:58 -0500

    Add login logout.

--- Ajourney/travelplans/views/view_plans.py
+++ Ajourney/travelplans/views/view_plans.py
@@ -43,12 +43,14 @@ def joined_plans(request):
 def view_plan_detail(request):
     planid=request.GET.get('planid')
     plan=get_plan_by_id(planid)
-    user=plan.get_holder()
-    is_friend=True
-    template = loader.get_template('travelplans/plan_detail.html')
-    context = RequestContext(request, {
+    template = loader.get_template('travelplans/')
+    if plan:
+        user=plan.get_holder()
+        is_friend=True
+        template = loader.get_template('travelplans/plan_detail.html')
+        context = RequestContext(request, {
         'plan': plan,
         'current_user': user,
         'is_friend':is_friend,
-    })
+        })
     return HttpResponse(template.render(context))
--- Ajourney/travelplans/views/views.py
+++ Ajourney/travelplans/views/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from travelplans.models import User
+from django.contrib.auth import login,authenticate
 import facebook
 import urllib2
 import json
@@ -21,27 +23,46 @@ def home(request):
 #       context = RequestContext(request,
 #                           {'request': request,
 #                           'user': request.user})
-        if hasattr(request.user, 'social_auth'):
-            social_user = request.user.social_auth.filter( provider='facebook',).first()
-            if social_user:
-                url = u'https://graph.facebook.com/{0}/' \
-                    u'friends?fields=id,name,location,picture' \
-                    u'&access_token={1}'.format(social_user.uid,
-                social_user.extra_data['access_token'],
-            )
-                response = urllib2.Request(url)
-                friends_json = json.loads(urllib2.urlopen(response).read()).get('data')
-                friends = []
-                for i in xrange(len(friends_json)):
-                    friends.append(FBuser(friends_json[i]['id'],friends_json[i]['name']))
-                #friends = friends_json[0]['id']
-                graph = facebook.GraphAPI(social_user.extra_data['access_token'])
-                profile = graph.get_object("me")
-                currentuser = FBuser(profile['id'],profile['name'])
-                context = RequestContext(request, {'request': request, 'user': request.user, 'friends': friends, 'currentuser': currentuser})
-                return render_to_response('travelplans/view_plans.html',context_instance=context)
-        else:
-                friends = None;
-        context = RequestContext(request, {'request': request, 'user': request.user, 'friends': friends})
+    friends = None
+    if hasattr(request.user, 'social_auth'):
+        social_user = request.user.social_auth.filter( provider='facebook',).first()
+        if social_user:
+            url = u'https://graph.facebook.com/{0}/' \
+                u'friends?fields=id,name,location,picture' \
+                u'&access_token={1}'.format(social_user.uid,
+            social_user.extra_data['access_token'],
+        )
+            response = urllib2.Request(url)
+            friends_json = json.loads(urllib2.urlopen(response).read()).get('data')
+            friends = []
+            for i in xrange(len(friends_json)):
+                friends.append(FBuser(friends_json[i]['id'],friends_json[i]['name']))
+            #friends = friends_json[0]['id']
+            graph = facebook.GraphAPI(social_user.extra_data['access_token'])
+            profile = graph.get_object("me")
+            currentuser = FBuser(profile['id'],profile['name'])
+            user_name=currentuser.name.split()
+            facebookid=currentuser.id
+            currentuser=User.objects.filter(username__exact=facebookid)
+            print "facebookid"
+            print facebookid
+            if len(currentuser)==0:
+                currentuser=User.objects.create_user(username=facebookid,password='',first_name=user_name[0],last_name=user_name[1])  
+            else:
+                currentuser=currentuser[0]
+            currentuser=authenticate(username=facebookid,password='')
+            if currentuser is not None: 
+                if currentuser.is_active:
+                    login(request,currentuser)
+            else:
+                print "user None"
+            return redirect('travelplans/')
 
-        return render_to_response('travelplans/home.html',context_instance=context)
+            '''
+            context = RequestContext(request, {'request': request, 'user': request.user, 'friends': friends, 'currentuser': currentuser})
+            return render_to_response('travelplans/view_plans.html',context_instance=context)
+            '''
+    else:
+        friends = None
+    context = RequestContext(request, {'request': request, 'user': request.user, 'friends': friends})
+    return render_to_response('travelplans/home.html',context_instance=context)
