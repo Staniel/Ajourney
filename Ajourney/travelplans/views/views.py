3b088db186569eadd9d8f530154034566e743c96
Yu Wang <yw2684@columbia.edu>
2014-11-09 02:53:20 -0500

    friends reference error fixed.

--- Ajourney/travelplans/views/views.py
+++ Ajourney/travelplans/views/views.py
@@ -22,28 +22,28 @@ def home(request):
 #       context = RequestContext(request,
 #                           {'request': request,
 #                           'user': request.user})
-        if hasattr(request.user, 'social_auth'):
-            social_user = request.user.social_auth.filter( provider='facebook',).first()
-            if social_user:
-                url = u'https://graph.facebook.com/{0}/' \
-                    u'friends?fields=id,name,location,picture' \
-                    u'&access_token={1}'.format(social_user.uid,
-                social_user.extra_data['access_token'],
-            )
-                response = urllib2.Request(url)
-                friends_json = json.loads(urllib2.urlopen(response).read()).get('data')
-                friends = []
-                for i in xrange(len(friends_json)):
-                    friends.append(FBuser(friends_json[i]['id'],friends_json[i]['name']))
-                #friends = friends_json[0]['id']
-                graph = facebook.GraphAPI(social_user.extra_data['access_token'])
-                profile = graph.get_object("me")
-                currentuser = FBuser(profile['id'],profile['name'])
-                plan_list=get_all_plans()
-                context = RequestContext(request, {'request': request, 'user': request.user, 'friends': friends, 'currentuser': currentuser, 'plan_list': plan_list, 'list_title':'All available plans' })
-                return render_to_response('travelplans/view_plans.html',context_instance=context)
-        else:
-                friends = None;
-        context = RequestContext(request, {'request': request, 'user': request.user, 'friends': friends})
-
-        return render_to_response('travelplans/home.html',context_instance=context)
+    friends = None
+    if hasattr(request.user, 'social_auth'):
+        social_user = request.user.social_auth.filter( provider='facebook',).first()
+        if social_user:
+            url = u'https://graph.facebook.com/{0}/' \
+                u'friends?fields=id,name,location,picture' \
+                u'&access_token={1}'.format(social_user.uid,
+            social_user.extra_data['access_token'],
+        )
+            response = urllib2.Request(url)
+            friends_json = json.loads(urllib2.urlopen(response).read()).get('data')
+            friends = []
+            for i in xrange(len(friends_json)):
+                friends.append(FBuser(friends_json[i]['id'],friends_json[i]['name']))
+            #friends = friends_json[0]['id']
+            graph = facebook.GraphAPI(social_user.extra_data['access_token'])
+            profile = graph.get_object("me")
+            currentuser = FBuser(profile['id'],profile['name'])
+            plan_list=get_all_plans()
+            context = RequestContext(request, {'request': request, 'user': request.user, 'friends': friends, 'currentuser': currentuser, 'plan_list': plan_list, 'list_title':'All available plans' })
+            return render_to_response('travelplans/view_plans.html',context_instance=context)
+    else:
+        friends = None
+    context = RequestContext(request, {'request': request, 'user': request.user, 'friends': friends})
+    return render_to_response('travelplans/home.html',context_instance=context)
