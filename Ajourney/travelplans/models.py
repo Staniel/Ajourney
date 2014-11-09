9492352c10f0adcfcd44358798f1ef18eac2e379
Ruibo Li <rl2733@columbia.edu>
2014-11-09 00:25:40 -0500

    Facebook ID

--- Ajourney/travelplans/models.py
+++ Ajourney/travelplans/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Plan(models.Model):
-	#holder = models.ForeignKey(User)
+	holder = models.ForeignKey(User)
 	description = models.CharField(max_length=500)
 	destination = models.CharField(max_length=200)
 	limit = models.IntegerField()
@@ -11,7 +11,7 @@ class Plan(models.Model):
 	def __str__(self):
 		return self.destination+'.'+self.description
 class JoinedPlan(models.Model):
-	#joined_user = models.ForeignKey(User)
+	joined_user = models.ForeignKey(User)
 	joined_plan = models.ForeignKey(Plan)
 	def __str__(self):
 		return self.joined_user.__str__()+" join "+self.joined_plan.__str__()
