698983acd6ff65c48fd15f1d54fdfcbf715b6986
Lixin Yao <Staniel@Lixins-MacBook-Pro.local>
2014-11-10 12:44:09 -0500

    add sample test

--- Ajourney/travelplans/tests.py
+++ Ajourney/travelplans/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class Tests(TestCase):
+
+    def test_sample(self):
+        """
+        sample test case
+        """
+        a = False
+        self.assertEqual(a, True)
