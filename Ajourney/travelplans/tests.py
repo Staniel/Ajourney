86c2ef3ddae0f3993fb3e978f3ed539286c66525
Lixin Yao <Staniel@Lixins-MacBook-Pro.local>
2014-11-10 12:51:13 -0500

    sample test case

--- Ajourney/travelplans/tests.py
+++ Ajourney/travelplans/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
-
+from travelplans.plan_manager import PlanManager
 # Create your tests here.
-class Tests(TestCase):
+class PlanManagerTestCase(TestCase):
 
-    def test_sample(self):
+    def test_planmanager_get_by_id_plan(self):
         """
         sample test case
         """
-        a = False
-        self.assertEqual(a, True)
+        pm = PlanManager()
+        plan = pm.get_plan_by_id(999)
+        self.assertEqual(plan, None)
