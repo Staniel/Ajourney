17a98e247b0b04cce3095118dc0881f6b828fd3a
Ruibo Li <rl2733@columbia.edu>
2014-11-10 14:08:35 -0500

    facebook test

--- Ajourney/travelplans/tests.py
+++ Ajourney/travelplans/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from travelplans.plan_manager import PlanManager
+import travelplans.facebook_proxy
+from django.contrib.auth.models import User
 # Create your tests here.
 class PlanManagerTestCase(TestCase):
 
@@ -10,3 +12,30 @@ class PlanManagerTestCase(TestCase):
         pm = PlanManager()
         plan = pm.get_plan_by_id(999)
         self.assertEqual(plan, None)
+
+
+"""There are 4 users in the database: Alice, Bob, Cathy, David"""
+"""Alice, Bob, David are friends, and Cathy is not friend of the aboves"""
+class FacebookProxyTestCase(TestCase):
+	def test_is_friend_valid(self):
+		Alice = User.objects.get(username__exact='Alice')
+		Bob = User.objects.get(username__exact='Bob')
+		ret = travelplans.facebook_proxy.is_friend(Alice, Bob)
+		self.assertTrue(ret)
+		ret = travelplans.facebook_proxy.is_friend(Alice, Bob)
+		self.assertTrue(ret)
+
+	def test_all_friends_valid(self):
+		Alice = User.objects.get(username__exact='Alice')
+		Bob = User.objects.get(username__exact='Bob')
+		David = User.objects.get(username__exact='David')
+		David = User.objects.get()
+		flist = all_friends(alice);
+			self.assertTrue(len(flist) == 2 and Bob in flist and David in list)
+
+	def test_all_friends_exception(self):
+		Cathy = User.obejects.get(username__exact='Cathy')
+		self.assertRaises(NoFriendsException,all_friends,Csathy)
+
+
+
