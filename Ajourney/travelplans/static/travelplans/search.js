63c1a81bb0849894e292095ba794d5390e429061
Lixin Yao <Staniel@Lixins-MacBook-Pro.local>
2014-11-15 20:15:54 -0500

    fix name bug in search box, change name of search label

--- Ajourney/travelplans/static/travelplans/search.js
+++ Ajourney/travelplans/static/travelplans/search.js
@@ -2,14 +2,13 @@ $(document).ready(function() {
 
     $('#searchbutton').click(function(e) {
         e.preventDefault();
-        // console.log(1);
         var dest = $('#destinationfilter').val().toLowerCase();
         var depart = $('#departfilter').val();
         var ret = $('#returnfilter').val();
         $('#plantable .plan_tr').each(function() {
             $(this).show();
             if (dest != '') {
-                var test_dest = $(this).find('.plan_dest').html().toLowerCase();
+                var test_dest = $(this).find('.plan_dest').text().toLowerCase();
                 if (test_dest.indexOf(dest) <= -1) {
                     $(this).hide();
                 }
@@ -20,14 +19,14 @@ $(document).ready(function() {
                 var test_depart = $(this).find('.plan_dep').html();
                 var arr = test_depart.split("-");
                 var this_date = new Date(arr[2], arr[0] - 1, arr[1]);
-
                 if (this_date < filter_date) {
                     $(this).hide();
                     return true;
                 }
             }
             if (ret != '') {
-                var filter_date = new Date(ret);
+                var temp_arr = ret.split("-");
+                var filter_date = new Date(temp_arr[0], temp_arr[1]-1, temp_arr[2]);
                 var test_return = $(this).find('.plan_ret').html();
                 var arr = test_return.split("-");
                 var this_date = new Date(arr[2], arr[0] - 1, arr[1]);
