6e49d9a886e3941b44076930b4f9d89a8c8b4d1c
Ruibo Li <rl2733@columbia.edu>
2014-11-23 23:30:53 -0500

    test

--- Ajourney/travelplans/tests.py
+++ Ajourney/travelplans/tests.py
@@ -2,85 +2,105 @@ from django.test import TestCase
 from travelplans.plan_manager import PlanManager
 import travelplans.facebook_proxy
 from datetime import datetime
+import pytz
 from travelplans.models import Plan
 from django.contrib.auth.models import User
 from django.test.client import RequestFactory
 from django.contrib.auth import authenticate, login
 
-# Create your tests here.
-# class PlanManagerTestCase(TestCase):
+#   Create your tests here.
+class PlanManagerTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', email='test@example.com', password='top_secret', is_active=True)
+        self.user.set_password('hello') 
+        self.user.save()
+        self.plan1 = Plan.objects.create(holder = self.user, description="test description", destination="west place", limit = 3, depart_time = datetime(2014,9,1,0,0,0), return_time = datetime(2014,9,1,0,0,0))
+        self.plan2 = Plan.objects.create(holder = self.user, description="test description", destination="west place", limit = 5, depart_time = datetime.today(), return_time = datetime.today())
+        self.user = authenticate(username='testuser', password='hello')
+        login = self.client.login(username='testuser', password='hello')
+        self.assertTrue(login)
 
-#     def test_planmanager_get_by_id(self):
-#         """
-#         get plan by id test case
-#         """
-#         pm = PlanManager()
-#         plan = pm.get_plan_by_id(999)
-#         self.assertEqual(plan, None)
-#     def test_planmanager_get_by_destination(self):
-#         """
-#         get plan by destination test case
-#         """
-#         pm = PlanManager()
-#         plan = pm.get_plans_by_destination("Beijing")
-#         self.assertEqual(plan, None)
-#     def test_planmanager_get_by_time(self):
-#         """
-#         get plan by time test case
-#         """
-#         pm = PlanManager()
-#         plan = pm.get_plans_by_time()
-#         self.assertEqual(plan, None)
-#     def test_planmanager_get_by_user(self):
-#         """
-#         get plan by user test case
-#         """
-#         pm = PlanManager()
-#         user = User.objects.get(id=1)
-#         plan = pm.get_plan_by_user(user)
-#         self.assertEqual(plan, None)
-#     def test_planmanager_get_all_plans(self):
-#         """
-#         sample test case
-#         """
-#         pm = PlanManager()
-#         user = User.objects.get(id=1)
-#         plans = pm.get_all_plans()
-#         self.assertEqual(len(plans), 3)
-#     def test_planmanager_get_joined_plans(self):
-#         """
-#         sample test case
-#         """
-#         pm = PlanManager()
-#         user = User.objects.get(id=1)
-#         plan = pm.get_joined_plans()
-#         self.assertEqual(len(plan), 4)
-#     def test_planmanager_has_joined_plans(self):
-#         """
-#         sample test case
-#         """
-#         self.assertTrue(True)
+    def test_planmanager_get_by_id(self):
+        """
+        get plan by id test case
+        """
+        pm = PlanManager()
+        plan_none = pm.get_plan_by_id(999)
+        self.assertEqual(plan_none, None)
+        plan_1 = pm.get_plan_by_id(1)
+        self.assertEqual(plan_1.holder, self.user)
+        self.assertEqual(plan_1.description, "test description")
+        self.assertEqual(plan_1.destination, "west place")
+        self.assertEqual(plan_1.limit, 3)
+        self.assertEqual(plan_1.depart_time,  pytz.utc.localize(datetime(2014,9,1,0,0,0)))
+        self.assertEqual(plan_1.return_time,  pytz.utc.localize(datetime(2014,9,1,0,0,0)))
 
-#     def test_planmanager_viewable(self):
-#         """
-#         sample test case
-#         """
-#         self.assertTrue(False)
-#     def test_planmanager_editable(self):
-#         """
-#         sample test case
-#         """
-#         self.assertTrue(False)
-#     def test_planmanager_shareable(self):
-#         """
-#         sample test case
-#         """
-#         self.assertTrue(False)
-#     def test_planmanager_joinable(self):
-#         """
-#         sample test case
-#         """
-#         self.assertTrue(False)
+
+
+    # def test_planmanager_get_by_destination(self):
+    #     """
+    #     get plan by destination test case
+    #     """
+    #     pm = PlanManager()
+    #     plan = pm.get_plans_by_destination("Beijing")
+    #     self.assertEqual(plan, None)
+    # def test_planmanager_get_by_time(self):
+    #     """
+    #     get plan by time test case
+    #     """
+    #     pm = PlanManager()
+    #     plan = pm.get_plans_by_time()
+    #     self.assertEqual(plan, None)
+    # def test_planmanager_get_by_user(self):
+    #     """
+    #     get plan by user test case
+    #     """
+    #     pm = PlanManager()
+    #     user = User.objects.get(id=1)
+    #     plan = pm.get_plan_by_user(user)
+    #     self.assertEqual(plan, None)
+    # def test_planmanager_get_all_plans(self):
+    #     """
+    #     sample test case
+    #     """
+    #     pm = PlanManager()
+    #     user = User.objects.get(id=1)
+    #     plans = pm.get_all_plans()
+    #     self.assertEqual(len(plans), 3)
+    # def test_planmanager_get_joined_plans(self):
+    #     """
+    #     sample test case
+    #     """
+    #     pm = PlanManager()
+    #     user = User.objects.get(id=1)
+    #     plan = pm.get_joined_plans()
+    #     self.assertEqual(len(plan), 4)
+    # def test_planmanager_has_joined_plans(self):
+    #     """
+    #     sample test case
+    #     """
+    #     self.assertTrue(True)
+
+    # def test_planmanager_viewable(self):
+    #     """
+    #     sample test case
+    #     """
+    #     self.assertTrue(False)
+    # def test_planmanager_editable(self):
+    #     """
+    #     sample test case
+    #     """
+    #     self.assertTrue(False)
+    # def test_planmanager_shareable(self):
+    #     """
+    #     sample test case
+    #     """
+    #     self.assertTrue(False)
+    # def test_planmanager_joinable(self):
+    #     """
+    #     sample test case
+    #     """
+    #     self.assertTrue(False)
 
 
 # """There are 4 users in the database: Alice, Bob, Cathy, David"""
@@ -131,25 +151,41 @@ from django.contrib.auth import authenticate, login
 #         self.assertTrue(False)
 
 
-class ManipulatePlanTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username='testuser', email='test@example.com', password='top_secret', is_active=True)
-        self.user.set_password('hello') 
-        self.user.save()
-        self.plan1 = Plan.objects.create(holder = self.user, description="test description", destination="west place", limit = 3, depart_time = datetime.today(), return_time = datetime.today())
-        self.plan2 = Plan.objects.create(holder = self.user, description="test description", destination="west place", limit = 5, depart_time = datetime.today(), return_time = datetime.today())
-        self.user = authenticate(username='testuser', password='hello')
-        login = self.client.login(username='testuser', password='hello')
-        self.assertTrue(login)
-    def test_delete_plans(self):
-        pm = PlanManager()
-        plan_list = pm.get_plans_by_user(self.user)
-        self.assertEqual(len(plan_list), 2)
-        response = self.client.post('/travelplans/delete_plan/1/', {})
-        self.assertEqual(response.status_code, 200)
-        print response
-        plan_list = pm.get_plans_by_user(self.user)
-        self.assertEqual(len(plan_list), 1)
+# class ManipulatePlanTestCase(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create(username='testuser', email='test@example.com', password='top_secret', is_active=True)
+#         self.user.set_password('hello') 
+#         self.user.save()
+#         self.plan1 = Plan.objects.create(holder = self.user, description="test description", destination="west place", limit = 3, depart_time = datetime.today(), return_time = datetime.today())
+#         self.plan2 = Plan.objects.create(holder = self.user, description="test description", destination="west place", limit = 5, depart_time = datetime.today(), return_time = datetime.today())
+#         self.user = authenticate(username='testuser', password='hello')
+#         login = self.client.login(username='testuser', password='hello')
+#         self.assertTrue(login)
+
+#     def test_delete_plans(self):
+#         pm = PlanManager()
+#         plan_list = pm.get_plans_by_user(self.user)
+#         self.assertEqual(len(plan_list), 2)
+#         response = self.client.post('/travelplans/delete_plan/1/', {})
+#         self.assertEqual(response.status_code, 200)
+#         print response
+#         plan_list = pm.get_plans_by_user(self.user)
+#         self.assertEqual(len(plan_list), 1)
+
+#     def test_edit_plans(self):
+#         pm = PlanManager()
+#         plan_list = pm.get_plans_by_user(self.user)
+#         #   self.assertEqual(len(plan_list), 2)
+#         response = self.client.post('/travelplans/edit_plan/1/', {})
+#         #self.assertEqual(response.status_code, 200)
+
+#     def test_create_plans(self):
+#         pm = PlanManager()
+#         plan_list = pm.get_plans_by_user(self.user)
+#         #   self.assertEqual(len(plan_list), 2)
+#         response = self.client.post('/travelplans/create_plan/', {})
+#         self.assertEqual(response.status_code, 200)
+
 
 
         
